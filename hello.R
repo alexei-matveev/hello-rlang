51b919bab542453e739fe7f1006915f07f91de40
Alexei Matveev <alexei.matveev@gmail.com>
2020-12-06 11:11:27 +0100

    AM: more options

--- hello.R
+++ hello.R
@@ -11,29 +11,43 @@
 ## In this  particular distribution  the increments are  usually small
 ## and positive or large and negative:
 prior <- c(1,2,3,4,4,4,5,7,7,-10,-20);
+
+## This outputs the frequency table:
+table (prior);
 mean (prior);                           # 0.6363636
+## plot (ecdf (prior), do.points = FALSE, verticals = TRUE);
 
 ## In case you want to exclude drift:
 ## prior <- prior - mean (prior);
 
-
-## This outputs the frequency table:
-table (prior);
-
 ## There are 7 * 24 = 168 hours in a week
-week <- 7 * 24;
+day <- 24;
+week <- 7 * day;
 usage <- cumsum (sample (prior, size = week, replace = TRUE));
+par (pty = "s");                        # square windows
 plot (usage, type = "l");
 
-## Simulate the eveolution over one week  many times. One year of data
-## is amounts to 52 one-week simulations, this 52 * 10 is ten years of
-## data! You will never have that much in reality!
-sims <- replicate (n = 52 * 10, sum (sample (prior, size = week, replace = TRUE)));
+## One week of  data, in fact only  167 = 168 -  1 differences, should
+## give us  some idea of prior  distribution. Still it will  often not
+## suffice to recover the prior accurately.
+distr <- diff (usage);
+table (distr);
+mean (distr);
+# plot (table (distr));
 
+## Simulate the evolution over one day  many times. In fact you may be
+## interested in the behaviour of max(cumsum(...)) or min(cumsum(...))
+## and not just sum(...):
+sims <- replicate (n = 100000, sum (sample (distr, size = day, replace = TRUE)));
+fivenum (sims);
 ## The expectation value of a sum is  a multiple of that for the prior
 ## distribution:
-c (mean (sims), week * mean (prior));
+c (mean (sims),  day * mean (distr), day * mean (prior));
 
 ## Histogram
-hist (sims, breaks=25);
+hist (sims, breaks = 50);           # , prob = TRUE);
+## lines (density (sims, bw = 5));
 
+## Empirical cumulative distribution function:
+## plot (ecdf (sims), do.points = FALSE, verticals = TRUE);
+## qqnorm (sims); qqline (sims);
